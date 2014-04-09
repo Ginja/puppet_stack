9561c01d7353e17e605d23344677c947a5689717
Riley Shott <rileyshott@gmail.com>
2014-04-09 02:18:46 -0700

    Fixed "Only root can execute commands as other users" errors for `rake spec`

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,12 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
+  c.before do
+    # Thanks, maestrodev
+    # avoid "Only root can execute commands as other users" errors
+    Puppet.features.stubs(:root? => true)
+  end
+
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
 end
