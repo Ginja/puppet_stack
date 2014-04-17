e18a1503d1df8321b335154648bd59c68623317e
Riley Shott <rileyshott@gmail.com>
2014-04-17 11:42:10 -0700

    Modified environment_spec.rb
    Added new test for ensure => absent
    Reorganized tests to match the style in puppet_stack_spec.rb

--- spec/defines/environment_spec.rb
+++ spec/defines/environment_spec.rb
@@ -1,24 +1,6 @@
 require 'spec_helper'
 
 describe 'puppet_stack::puppet::environment', :type => :define do
-  let :facts do
-    {
-      :puppetversion          => '3.4.3',
-      :rubyversion            => '2.0.0',
-      :osfamily               => 'RedHat',
-      :operatingsystemrelease => '6',
-      :concat_basedir         => '/dne',
-    }
-  end
-  let :pre_condition do
-    'class { \'puppet_stack\':
-       ruby_vers      => \'ruby-2.0.0-p451\',
-       passenger_vers => \'4.0.40\',
-       foreman        => false,
-       smartproxy     => false,
-     }'
-  end
-  
   context "with an illegal environment name" do
     unsupported_examples = [nil, 'main', 'master', 'agent', 'user', '*magic']
     random_env_name = unsupported_examples[rand(unsupported_examples.length - 1)]
@@ -47,48 +29,98 @@ describe 'puppet_stack::puppet::environment', :type => :define do
     it { expect { should compile }.to raise_error(/one of the following values: present, absent/) }
   end
   
-  context "on any supported system" do
-    let :title do
-      'production'
-    end
-    let :params do
+  context "on a RedHat system" do
+    let :facts do
       {
-        :ensure => 'present',
+        :puppetversion          => '3.4.3',
+        :rubyversion            => '2.0.0',
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '6',
+        :concat_basedir         => '/dne',
       }
     end
-    it {
-         should compile.with_all_deps
-         should contain_class('puppet_stack::puppet')
-         should contain_file('/etc/puppet').with({
-           'ensure' => 'directory',
-           'owner'  => 'root',
-           'group'  => 'root',
-           'mode'   => '0755',
-         })
-         should contain_file('/etc/puppet/environments').with({
-           'ensure' => 'directory',
-           'owner'  => 'root',
-           'group'  => 'puppet',
-           'mode'   => '0755',
-         })
-         should contain_file('/etc/puppet/environments/production').with({
-           'ensure' => 'directory',
-           'owner'  => 'root',
-           'group'  => 'puppet',
-           'mode'   => '0755',
-         })
-         should contain_file('/etc/puppet/environments/production/modules').with({
-           'ensure' => 'directory',
-           'owner'  => 'root',
-           'group'  => 'puppet',
-           'mode'   => '0755',
-         })
-         should contain_file("/etc/puppet/environments/production/manifests").with({
-           'ensure' => 'directory',
-           'owner'  => 'root',
-           'group'  => 'puppet',
-           'mode'   => '0755',
-         })
-       }
+    let :pre_condition do
+      'class { \'puppet_stack\':
+         ruby_vers      => \'ruby-2.0.0-p451\',
+         passenger_vers => \'4.0.40\',
+         foreman        => false,
+         smartproxy     => false,
+       }'
+    end
+    
+    context "with ensure => present" do
+      let :title do
+        'production'
+      end
+      let :params do
+        {
+          :ensure => 'present',
+        }
+      end
+      it {
+           should compile.with_all_deps
+           should contain_class('puppet_stack::puppet')
+           should contain_file('/etc/puppet').with({
+             'ensure' => 'directory',
+             'owner'  => 'root',
+             'group'  => 'root',
+             'mode'   => '0755',
+           })
+           should contain_file('/etc/puppet/environments').with({
+             'ensure' => 'directory',
+             'owner'  => 'root',
+             'group'  => 'puppet',
+             'mode'   => '0755',
+           })
+           should contain_file('/etc/puppet/environments/production').with({
+             'ensure' => 'directory',
+             'owner'  => 'root',
+             'group'  => 'puppet',
+             'mode'   => '0755',
+           })
+           should contain_file('/etc/puppet/environments/production/modules').with({
+             'ensure' => 'directory',
+             'owner'  => 'root',
+             'group'  => 'puppet',
+             'mode'   => '0755',
+           })
+           should contain_file("/etc/puppet/environments/production/manifests").with({
+             'ensure' => 'directory',
+             'owner'  => 'root',
+             'group'  => 'puppet',
+             'mode'   => '0755',
+           })
+         }
+    end
+    
+    context "with ensure => absent" do
+      let :title do
+        'production'
+      end
+      let :params do
+        {
+          :ensure => 'absent',
+        }
+      end
+      it {
+        should compile.with_all_deps
+        should contain_class('puppet_stack::puppet')
+        should contain_file('/etc/puppet').with({
+          'ensure' => 'directory',
+          'owner'  => 'root',
+          'group'  => 'root',
+          'mode'   => '0755',
+        })
+        should contain_file('/etc/puppet/environments').with({
+          'ensure' => 'directory',
+          'owner'  => 'root',
+          'group'  => 'puppet',
+          'mode'   => '0755',
+        })
+        should contain_file('/etc/puppet/environments/production')
+        should_not contain_file('/etc/puppet/environments/production/modules')
+        should_not contain_file("/etc/puppet/environments/production/manifests")
+      }
+    end
   end
 end
